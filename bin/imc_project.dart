21eab4d6459a645c42510c3f1fd1d5d212634afa
stonefullstm <stonefull.stm@gmail.com>
2025-01-17 17:00:33 -0300

    Commit inicial

---
+++ bin/imc_project.dart
@@ -0,0 +1,5 @@
+import 'package:imc_project/imc_project.dart' as imc_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imc_project.calculate()}!');
+}
---
+++ test/imc_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:imc_project/imc_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
