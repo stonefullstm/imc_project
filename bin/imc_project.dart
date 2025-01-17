7813a0d699e051171ec9ced021f6d048f4b803ce
stonefullstm <stonefull.stm@gmail.com>
2025-01-17 18:36:35 -0300

    feat: Cria função execute() para rodar o programa

--- bin/imc_project.dart
+++ bin/imc_project.dart
@@ -1,7 +1,6 @@
-//import 'package:imc_project/imc_project.dart' as imc_project;
-import 'package:imc_project/classes/pessoa.dart';
+import 'package:imc_project/imc_project.dart' as imc_project;
+// import 'package:imc_project/classes/pessoa.dart';
 
 void main(List<String> arguments) {
-  var pessoa = Pessoa("Joao da Silva", 72.0, 1.80);
-  print(pessoa);
+  imc_project.execute();
 }
--- lib/imc_project.dart
+++ lib/imc_project.dart
@@ -1,3 +1,19 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:imc_project/classes/pessoa.dart';
+
+void execute() {
+  print("Seja bemvindo à calculadora de IMC");
+  print("----------------------------------");
+  print("Digite o nome da pessoa:");
+  String nome = stdin.readLineSync(encoding: utf8) ?? "";
+  print("Digite o peso da pessoa (kg):");
+  var value = stdin.readLineSync(encoding: utf8) ?? "";
+  double peso = double.parse(value);
+  print("Digite a altura da pessoa (m):");
+  value = stdin.readLineSync(encoding: utf8) ?? "";
+  double altura = double.parse(value);
+  Pessoa pessoa = Pessoa(nome, peso, altura);
+  print(pessoa);  
 }
