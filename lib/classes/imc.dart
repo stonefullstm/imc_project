c50e6c18b475fd15b5be86f38c6b0c7ffbac9df9
stonefullstm <stonefull.stm@gmail.com>
2025-01-23 17:35:14 -0300

    feat: Cria classe Imc

---
+++ lib/classes/imc.dart
@@ -0,0 +1,28 @@
+import 'package:imc_project/classes/pessoa.dart';
+
+class Imc {
+  static double calculateImc(Pessoa pessoa) {
+    return pessoa.getPeso() / (pessoa.getAltura() * pessoa.getAltura());   
+  }
+
+  static String resultImc(Pessoa pessoa) {
+    var imc = calculateImc(pessoa);
+    if (imc < 16) {
+      return "Magreza grave";
+    } else if (imc < 17) {
+      return "Magreza moderada"; 
+    } else if (imc < 18.5) {
+      return "Magreza leve";
+    } else if (imc < 25) {
+      return "Saudável";
+    } else if (imc < 30) {
+      return "Sobrepeso";
+    } else if (imc < 35) {
+      return "Obesidade grau I";
+    } else if (imc < 40) {
+      return "Obesidade grau II (severa)";
+    } else {
+      return "Obesidade grau III (mórbida)";
+    }
+  }
+}
